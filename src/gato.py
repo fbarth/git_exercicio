3707054b0f65f90902b323c9d0d1ee763e82243b
Igor Montagner <igordsm@gmail.com>
2022-05-10 09:25:54 -0300

    estrando projeto

--- src/gato.py
+++
@@ -1,7 +0,0 @@
-class Gato():
-
-    def __init__(self, nome):
-        self.nome = nome
-
-    def fala_algo(self):
-        return f'Miau miau miau'
--- src/pessoa.py
+++
@@ -1,8 +0,0 @@
-class Pessoa():
-
-    def __init__(self, nome, idade):
-        self.nome = nome
-        self.idade = idade
-
-    def fala_algo(self):
-        return f'Oi! Meu nome é {self.nome}!'
